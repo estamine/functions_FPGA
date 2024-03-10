cf9661894fbdd3a73987da5277047ffbae65c734
Rui Caldeira <151440915+estamine@users.noreply.github.com>
2024-03-10 18:00:21 +0100

    Add files via upload

---
+++ Frequency_Divider/divide_freq.v
@@ -0,0 +1,17 @@
+module divide_freq(clki,clko);
+
+	input clki;
+	output clko;
+	reg [24:0] conta;
+	reg clko;  
+
+   always @(posedge clki)
+     
+	begin
+	
+	conta <= conta + 1;
+	
+	clko <= conta[24];
+	
+	end
+endmodule
